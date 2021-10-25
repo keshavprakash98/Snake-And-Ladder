6ea3c67cc14f59349146f26bc0cea1a42a2cab9e
keshavprakash98 <keshavprakash98@gmail.com>
2021-10-25 22:25:14 +0530

    snakeAndLadder

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace snakeAndladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Position = 0;
+            Console.WriteLine("Player is at starting  Position: " + Position);
+        }
+    }
+}
